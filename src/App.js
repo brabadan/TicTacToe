48b78028ef755b59ea4e32e24c2223dbd2df65fc
Rabadan Bakhmudov <brabadan@yandex.ru>
2018-11-08 19:01:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TicTacToe from './components/TicTacToe';
+import StartPage from './components/StartPage';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        showStartPage: true // Условие показа стартовой страницы
+    };
+
+    // Функция - Закрывает стартовую страницу
+    closeStartPage() {
+        this.setState({showStartPage: false});
+    }
+
+    render() {
+        // Если надо - показываем стартовую страницу
+        if (this.state.showStartPage) {
+            return (
+                <div className="App-start-page"
+                     onClick={this.closeStartPage.bind(this)}
+                >
+                    <StartPage/>
+                </div>
+            )
+        } else // Иначе показываем основную страницу
+            return (
+                <div className="App">
+                    {/*шапка*/}
+                    <header className="App-header">
+                        <h6>TIC-TAC-TOE === КРЕСТИКИ-НОЛИКИ</h6>
+                    </header>
+                    {/*ИГРА*/}
+                    <TicTacToe cellSize="60"/>
+                </div>
+            );
+    }
 }
 
 export default App;
